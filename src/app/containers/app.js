2946c8251545e785a6f228f0d7b27bc2c07a3f43
Keith Yeung <kmyeung@andrew.cmu.edu>
2017-01-11 23:30:50 -0500

    initial working config

---
+++ src/app/containers/app.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Header} from '../components/Header';
+
+export class app extends Component {
+
+	render() {
+		return (
+			<div>
+				<Header/>
+			</div>
+		);
+	}
+
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
-
-import {Hello} from './app/hello';
+import {app} from './app/containers/app';
 
 import './index.scss';
 
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={Hello}/>
-  </Router>,
+	<Router history={browserHistory}>
+		<Route path="/" component={app}/>
+	</Router>,
   document.getElementById('root')
 );
