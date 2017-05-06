cd2d2fd5f8f9faa4b6d03a9dea53bc985d163f16
Keith Yeung <kmyeung@andrew.cmu.edu>
2017-05-06 01:22:50 -0400

    homepage finished

--- src/app/components/Home2.js
+++ src/app/components/Home2.js
@@ -8,6 +8,10 @@ export class Home2 extends Component {
 	render() {
 		return (
 			<div className="Home2">
+				<div className="HomeImage">
+					<img src="../images/home_opt.png"/>
+					<div className="HomeImageOpacity"/>
+				</div>
 				<ReactSVG path="../images/logo.svg" className="logoHome"/>
 				<div className="HomeWords">
 					<div className="HomeTitle">
